88c836247b648c6bc739c15e733f4f8320046c37
ZiaadNegm <ziaad.negmel-din@student.uva.nl>
2025-11-08 14:15:08 +0100

    inital commit

---
+++ bigram/bigram.py
@@ -0,0 +1,10 @@
+import numpy as np
+
+matrix = np.random.rand(5, 5)
+
+print(matrix)
+
+# for int, _ in enumerate(matrix):
+#    print(matrix[1, :])
+    matrix[matrix > 0.5]
+print(matrix[matrix > 0])
